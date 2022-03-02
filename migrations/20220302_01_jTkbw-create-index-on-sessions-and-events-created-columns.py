6d288117aa41c41543dfe30733efb33fb454a5bd
Arco Mul <me@arcomul.nl>
2022-03-02 10:20:17 +0100

    Create indexes concurrently

--- migrations/20220302_01_jTkbw-create-index-on-sessions-and-events-created-columns.py
+++ migrations/20220302_01_jTkbw-create-index-on-sessions-and-events-created-columns.py
@@ -5,8 +5,15 @@ Create index on sessions and events created columns
 from yoyo import step
 
 __depends__ = {'20220109_01_initial-database-schema'}
+__transactional__ = False
 
 steps = [
-    step("CREATE INDEX sessions_created_brin_index ON sessions USING BRIN (created);"),
-    step("CREATE INDEX events_created_brin_index ON events USING BRIN (created);")
+    step(
+        "CREATE INDEX CONCURRENTLY sessions_created_brin_index ON sessions USING BRIN (created);",
+        "DROP INDEX sessions_created_brin_index;"
+    ),
+    step(
+        "CREATE INDEX CONCURRENTLY events_created_brin_index ON events USING BRIN (created);",
+        "DROP INDEX events_created_brin_index;"
+    )
 ]
